845352d008d3c9cc28d4e3b910ca3879d83d37d9
hainet <20400616+hainet@users.noreply.github.com>
2018-04-25 10:11:58 +0900

    Polish

--- src/main/java/com/hainet/spring/mapstruct/sample/mapper/AnnotatedDecorationMapper.java
+++ src/main/java/com/hainet/spring/mapstruct/sample/mapper/AnnotatedDecorationMapper.java
@@ -4,21 +4,17 @@ import com.hainet.spring.mapstruct.sample.annotation.Decoration;
 import com.hainet.spring.mapstruct.sample.annotation.Emphasize;
 import com.hainet.spring.mapstruct.sample.annotation.Strike;
 import org.mapstruct.Mapper;
-import org.mapstruct.Named;
 
 @Mapper
 @Decoration
-@Named("Decoration")
 public interface AnnotatedDecorationMapper {
 
     @Emphasize
-    @Named("Emphasize")
     default String emphasize(String value) {
         return "**" + value + "**";
     }
 
     @Strike
-    @Named("Strike")
     default String Strike(String value) {
         return "~~" + value + "~~";
     }
