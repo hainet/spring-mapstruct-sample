e0ff60b880dee18c414b28cda001dcd3ca151b11
hainet <20400616+hainet@users.noreply.github.com>
2018-04-30 19:19:25 +0900

    Remove verbose mapping on updating entity with @InheritConfiguration.

--- src/main/java/com/hainet/spring/mapstruct/sample/mapper/EntityMapper.java
+++ src/main/java/com/hainet/spring/mapstruct/sample/mapper/EntityMapper.java
@@ -2,10 +2,7 @@ package com.hainet.spring.mapstruct.sample.mapper;
 
 import com.hainet.spring.mapstruct.sample.entity.Entity;
 import com.hainet.spring.mapstruct.sample.model.EntityModel;
-import org.mapstruct.Mapper;
-import org.mapstruct.Mapping;
-import org.mapstruct.MappingTarget;
-import org.mapstruct.Mappings;
+import org.mapstruct.*;
 
 @Mapper(uses = NestedEntityMapper.class)
 public interface EntityMapper {
@@ -17,10 +14,6 @@ public interface EntityMapper {
     })
     EntityModel entityToModel(Entity entity);
 
-    @Mappings({
-            @Mapping(source = "nestedEntity", target = "nestedEntityModel"),
-            @Mapping(target = "defaultValue", defaultValue = "undefined"),
-            @Mapping(target = "constant", constant = "CONSTANT")
-    })
+    @InheritConfiguration
     void entityToModel(@MappingTarget EntityModel model, Entity entity);
 }
